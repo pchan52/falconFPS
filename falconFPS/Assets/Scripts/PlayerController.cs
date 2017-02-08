f355fc8285bec225d5b7d8ba7a947a5f07b84a50
pchan52 <pchan52611625@gmail.com>
2017-02-08 22:52:52 +0900

    Create player controller.cs to add squat action and down move speed

---
+++ falconFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerController : MonoBehaviour {
+
+	FirstPersonController fps;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.C)) {
+			print ("aa");
+			transform.position = new Vector3 (transform.position.x, 4f, transform.position.z);
+			gameObject.GetComponent<FirstPersonController>().m_WalkSpeed = 2f;
+		}
+		
+	}
+}
