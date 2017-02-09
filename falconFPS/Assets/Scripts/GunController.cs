31dd15fd8ad8df20e0dc14e6e742b10de581371d
pchan52 <pchan52611625@gmail.com>
2017-02-09 16:11:00 +0900

    Create shot function

--- falconFPS/Assets/Scripts/GunController.cs
+++ falconFPS/Assets/Scripts/GunController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour {
 
-
+	[SerializeField] private int bulletBox;
+	[SerializeField] private int bullet;
+	[SerializeField] private GameObject firePrefs;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,17 @@ public class GunController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+
+		Ray ray = new Ray(transform.position, transform.up);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (Input.GetMouseButton (0)) {
+				GameObject fire = Instantiate (firePrefs);
+				firePrefs.transform.position = hit.point;
+				Destroy (fire, 0.5f);
+
+			}
+		}
 	}
 		
 }
