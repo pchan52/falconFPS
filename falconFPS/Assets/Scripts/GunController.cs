2055d2cd5f2e1164f79abe077efef3da9d91b103
pchan52 <pchan52611625@gmail.com>
2017-02-09 16:22:18 +0900

    Edit code to be able to create shot sound

--- falconFPS/Assets/Scripts/GunController.cs
+++ falconFPS/Assets/Scripts/GunController.cs
@@ -8,8 +8,12 @@ public class GunController : MonoBehaviour {
 	[SerializeField] private int bullet;
 	[SerializeField] private GameObject firePrefs;
 
+	[SerializeField] private AudioClip shotSound;
+	AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
@@ -20,7 +24,8 @@ public class GunController : MonoBehaviour {
 		if (Physics.Raycast (ray, out hit)) {
 			if (Input.GetMouseButton (0)) {
 				GameObject fire = Instantiate (firePrefs);
-				firePrefs.transform.position = hit.point;
+				fire.transform.position = hit.point;
+				audioSource.PlayOneShot (shotSound);
 				Destroy (fire, 0.5f);
 
 			}
