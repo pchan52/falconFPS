c3ffe4e0fbee86d0c2c8e0c8e63bd61fb6a76730
pchan52 <pchan52611625@gmail.com>
2017-02-09 23:06:19 +0900

    Create TargetController.cs and Edit

---
+++ falconFPS/Assets/Scripts/TargetController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+	Animator anim;
+	private int hitCount;
+
+	// Use this for initialization
+	void Start () {
+		anim =  transform.parent.gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Hit(){
+		hitCount++;
+		print (hitCount);
+		if (hitCount == 5) {
+			anim.SetBool ("broken", true);
+			hitCount = 0;
+			Invoke("Revival", 10f);
+		}
+	}
+
+	private void Revival(){
+		anim.SetBool ("broken", false);
+	}
+
+}
