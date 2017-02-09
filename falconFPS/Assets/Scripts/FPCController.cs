564dfa6600868a96c322001cce9aa57792bc3f3b
pchan52 <pchan52611625@gmail.com>
2017-02-09 19:01:39 +0900

    Change name of script to be easily understood

--- falconFPS/Assets/Scripts/FPCController.cs
+++ falconFPS/Assets/Scripts/FPCController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class FPCController : MonoBehaviour {
 
 	[SerializeField] GameObject gunobj;
 	GunController gun;
