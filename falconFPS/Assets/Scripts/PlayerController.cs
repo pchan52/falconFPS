068f3f5e2fb9204e3f11ecc07140c9bc8f37e08c
pchan52 <pchan52611625@gmail.com>
2017-02-09 17:51:20 +0900

    Create PlayerController.cs to use ray

---
+++ falconFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] GameObject gunobj;
+	GunController gun;
+
+	// Use this for initialization
+	void Start () {
+		gun = gunobj.GetComponent<GunController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (Input.GetMouseButton (0) && !gun.isEmpty) {
+
+				gun.Shot (hit.point);
+				gun.UseBullet ();
+			}
+		}
+	}
+}
