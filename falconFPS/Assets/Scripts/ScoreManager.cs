dd74e20561d5af3f7b0410c9224384070cc37262
pchan52 <pchan52611625@gmail.com>
2017-02-10 17:57:26 +0900

    Edit ScoreManager.cs to add score function

--- falconFPS/Assets/Scripts/ScoreManager.cs
+++ falconFPS/Assets/Scripts/ScoreManager.cs
@@ -19,13 +19,28 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	public void CalcScore(Vector3 hitposition){
-		float distance = Vector3.Distance (hitposition, HeadMarker.transform.position);
-		if (distance == 0f) {
-			score += 100;
-		} else {
-			score += 100 / (int)distance; //距離が大きいほど加点が減る
-		}
+		Vector2 hitposition2 = hitposition; 
+		Vector2 headmarker2 = HeadMarker.transform.position;
+		float distance = Vector2.Distance (hitposition2, headmarker2); //x-y平面における距離にする
+		print(distance);
+		score += Score (distance);
 		print (score);
+	}
+
+	private int Score(float distance){
+		if (distance < 0.04f) {
+			return 100;
+		} else if (distance < 0.08f) {
+			return 90;
+		} else if (distance < 0.12f) {
+			return 80;
+		} else if (distance < 0.16f) {
+			return 70;
+		} else if (distance < 0.20f) {
+			return 60;
+		}else{
+			return 50;
+		}
 	}
 
 }
