9af1520301845e18f2afde1ee0be1d74f5faf13c
pchan52 <pchan52611625@gmail.com>
2017-02-10 11:12:37 +0900

    Create ScoreManager.cs to manage score

---
+++ falconFPS/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	[SerializeField] private GameObject HeadMarker;
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CalcScore(Vector3 hitposition){
+		float distance = Vector3.Distance (hitposition, HeadMarker.transform.position);
+		if (distance == 0f) {
+			score += 100;
+		} else {
+			score += 100 / (int)distance; //距離が大きいほど加点が減る
+		}
+		print (score);
+	}
+
+}
