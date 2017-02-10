7b0b3c16679ca7dc22cecb9f6aa44d78e3721ded
池田光 <Hikaru@pchan.local>
2017-02-10 22:06:05 +0900

    Delete unnecessary instance

--- falconFPS/Assets/Scripts/UIManager.cs
+++ falconFPS/Assets/Scripts/UIManager.cs
@@ -9,25 +9,22 @@ public class UIManager : MonoBehaviour {
 	[SerializeField] Text Score;
 	[SerializeField] Text Bullet;
 	[SerializeField] Text BulletBox;
-	[SerializeField] GameObject scoreManager;
-	[SerializeField] GameObject gunController;
+	[SerializeField] ScoreManager scoreManager;
+	[SerializeField] GunController gunController;
 
-	ScoreManager sm;
-	GunController gc;
 	float time;
 
 	// Use this for initialization
 	void Start () {
-		sm = scoreManager.GetComponent<ScoreManager> ();
-		gc = gunController.GetComponent<GunController> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time = 90.0f - Time.time;
 		LeftTime.text = "Time : " + time.ToString("N1") + "s";
-		Score.text = "Pt : " + sm.score;
-		Bullet.text = "Bullet : " + gc.bullet + "/30";
-		BulletBox.text = "BulletBox : " + gc.bulletBox;
+		Score.text = "Pt : " + scoreManager.score;
+		Bullet.text = "Bullet : " + gunController.bullet + "/30";
+		BulletBox.text = "BulletBox : " + gunController.bulletBox;
 	}
 }
